841bc0725c8536b7c96a8985bd9f3e0b1728da83
Nebojša Bogosavljević <nebojsabogosvljevic3@gmail.com>
2022-04-09 23:09:42 +0200

    Graph visualisation improved, added array to input.

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,7 @@
+from typing import List
 import matplotlib.pyplot as plt
-import numpy as np
+# from sympy.ntheory.primetest import is_square 
+import array as arr
 
 N_NUMBER = 256
 
@@ -10,33 +12,67 @@ def main(num):
 
 def single_graph(num, option=None):
     plot_array = []
-    c = 0; i = 0
+    c = 0; i = 0; max_c = [0, 0]
     while (True):
         if c == 0:
             c = num
+            plot_array.append([i-1, c])
+            plt.text(i-1, c, c)
         if c % 2 == 0:
             c = c / 2
             plot_array.append([i, c])
+            if c % 4 == 0 and c > max_c[0]:
+                max_c = [c, i, num]
+                plt.text(i, c, c)
+            else:
+                plt.text(i, c, c)
         else:
             c = c * 3 + 1
             plot_array.append([i, c])
+            if c % 4 == 0 and c > max_c[0]:
+                max_c = [c, i, num]
+                plt.text(i, c, c)
+            else:
+                plt.text(i, c, c)
         if c == 1:
+            plt.text(max_c[1], max_c[0], max_c[0], color="red")
             break;
         i += 1
     a, b = zip(*plot_array)
-    if (option != None):
-        return a, b
+    if (option == 1):
+        return a, b, max_c
     plt.plot(a, b, marker = "o", linestyle = "--")
     plt.show()
-
-def multiple_graphs(nums):
-    for num in nums:
-        a, b = single_graph(num)
-        plt.plot(a, b, marker = ".", linestyle = "--")
     
+def multiple_graphs(nums):
+    final_array = []; max_c = []
+    for num in nums:
+        a, b, max = single_graph(num, 1)
+        max_c.append(max)
+        plt.plot(a, b, marker = ".", markersize = 8, label=str(num))
+    print(str(max_c))
+    plt.legend()
+    plt.show()
+
+# def sort_by_num_max_value(max_c):
+#     a, b, c = zip(*max_c)
+#     unique_max = [set(a)]
+#     sort_list = list()
+#     for i in max_c:
+#         if i[1] in unique_max:
+            
+        
+        
 if __name__ == '__main__':
-    option = input("Enter a option: ")
-    if option == 1:
-        a = input("Enter a Value: ")
-        single_graph(input("Enter a Value: "))
-  
+    while(True):
+        option = raw_input("Enter a option: ")
+        if option == "done":
+            break;
+        elif option == '1':
+            single_graph(input("Enter a Value: "))
+        elif option == '2':
+            str_arr = raw_input('Enter values: ').split(' ')
+            arr = [int(num) for num in str_arr]
+            multiple_graphs(arr)
+        else:
+            print('Invalid Option, try again!')
